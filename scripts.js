b1f09a6c1ad1dc0c5d61099e8ab1fad7a400a763
kaymas <samyakahuja24@hotmail.com>
2017-12-29 13:18:20 +0530

    sidebar fix

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
 $(window).on('load',function(){
 
-  console.log("loaded");
+  // console.log("loaded");
 
   $('.nav-icon').on('click',function(){
     $(this).parents('header').find('.nav').toggleClass('is-open');
     $(this).toggleClass('is-open');
-    console.log("open");
+    // console.log("open");
   });
 
   $('.nav-line').find('.line-link').on('click',function(){
     $(this).parents('.nav-line').find('.line-link').removeClass('is-selected');
     $(this).addClass('is-selected');
   });
+
+  var height;
+  setInterval(function(){
+    height = $('.content-intro').height();
+    height += 40;
+    $('.nav').css({'height' :''+ height +'px'})
+  },1000);
 
 });
